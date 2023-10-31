e204d49908990f891f3daa0f962af226cb23cdf5
keizo.kita <leonardo.kita@amplaito.com.br>
2023-10-31 09:53:08 -0300

    metodo save funcionando

---
+++ src/main/java/com/keizo/TccProjectApplication.java
@@ -0,0 +1,29 @@
+package com.keizo;
+
+import com.keizo.model.Course;
+import com.keizo.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TccProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccProjectApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
+}
---
+++ src/main/java/com/keizo/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.keizo.controller;
+
+import com.keizo.model.Course;
+import com.keizo.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+}
